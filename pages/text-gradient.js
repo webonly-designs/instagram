b63b0a44ac10ac9ebfc997e266f9dc0600a9991d
Akshay Agarwal <agarwal.akshay003@gmail.com>
2022-12-15 20:01:01 -0600

    commit

---
+++ pages/text-gradient.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TextGradient() {
+  return (
+    <div className="bg-black py-20 flex justify-center">
+        <p className="font-extrabold text-transparent text-8xl 
+            bg-clip-text bg-gradient-to-r from-purple-600 to-rose-600">
+            <span>TailwindCss</span>
+        </p>
+    </div>
+  )
+}
+
+export default TextGradient
